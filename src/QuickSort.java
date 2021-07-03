4bbde7cf172bd17135487a3dad4fc00da16c7886
DamianKumar7 <59192428+DamianKumar7@users.noreply.github.com>
2021-07-03 13:51:38 +0530

    quiksort

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,54 +1,53 @@
-import java.util.*;
-class QuickSort{
-    public static void swap(int a,int b){
-        int temp = a;
-        a = b;
-        b = temp;
-    }
-    public static int partition(int[]arr,int low,int high){
-        int left = low;
-        int right = high;
-        int pivot = arr[low];
-        while(left<right){
-            do{
-                left++;
-            }
-            while(arr[left] <= pivot);
-            do{
-                right--;
+import java.util.Arrays;
+import java.util.Scanner;
 
-            }
-            while(arr[right] >pivot);
+class QuickSort {
+	 
 
-            if(left < right){
-                //swap(arr[left],arr[right]);
-                int temp = arr[left];
-                arr[left] = arr[right];
-                arr[right] = temp;
-            }
-            
-            
-        }
-        //swap(pivot,arr[right]);
-        int temp = pivot;
-        pivot = arr[right];
-        arr[right] = temp;
-        return right;
-    }
-    public static void quickSort(int[]arr,int low,int high){
-        if(low<high){
-            int pivot = partition(arr, low, high);
-            quickSort(arr, low, pivot);
-            quickSort(arr, pivot+1, high);
-        }
-    }
-     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] arr = {7,5,2,9,8,10};
-        int n = arr.length;
-        quickSort(arr,0,n-1);
-        for(int i = 0;i <n;i++){
-            System.out.println(arr[i]);
-        }
-    }
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter no of terms");
+		int n = sc.nextInt();
+
+		System.out.println("Enter the terms");
+		int arr[] = new int[n];
+		for (int i = 0; i < n; i++)
+			arr[i] = sc.nextInt();
+
+		System.out.println("The unsorted array is:");
+		System.out.println(Arrays.toString(arr));
+
+		quicksort(arr, 0, arr.length - 1);
+
+		System.out.println("The sorted array is:");
+		System.out.println(Arrays.toString(arr));
+	}
+
+	static void quicksort(int arr[], int start, int end) {
+		if (start < end) {
+			int pIndex = partition(arr, start, end);
+			quicksort(arr, start, pIndex - 1);
+			quicksort(arr, pIndex + 1, end);
+		}
+	}
+
+	static int partition(int arr[], int start, int end) {
+		int pivot = arr[end];
+		int pIndex = start;
+		for (int i = start; i < end; i++) {
+			if (arr[i] <= pivot) {
+				swap(arr, i, pIndex);
+				pIndex++;
+			}
+		}
+		swap(arr, pIndex, end);
+		return pIndex;
+	}
+
+	static void swap(int arr[], int x, int y) {
+		int temp = arr[x];
+		arr[x] = arr[y];
+		arr[y] = temp;
+	}
 }
